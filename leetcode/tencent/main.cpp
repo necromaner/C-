b6fa04cb158d0c6ee6c3c97c634cd90f9d00fce1
necromaner <necromaner@foxmail.com>
2018-09-09 15:59:50 +0800

    9.9

--- leetcode/tencent/main.cpp
+++ leetcode/tencent/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
-
+#include "Output.h"
+void executable(string folder,int file,vector<string> other){
+    //add_executable(T1_01 T1/T1.cpp Output.cpp)
+    for (int i = 1; i < file+1; ++i) {
+        printf("add_executable(%s_",folder.c_str());
+        if(i<10)
+            printf("0");
+        printf("%d %s/T%d.cpp",i,folder.c_str(),i);
+        for (int j = 0; j < other.size(); ++j) {
+            printf(" %s.cpp",other[j].c_str());
+        }
+        printf(")\n");
+    }
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string folder="T2";
+    int file=2;
+    vector<string> other={"Output"};
+    executable(folder,file,other);
     return 0;
 }
