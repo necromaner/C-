63423a9fee35d7e8eb5c15d37578540e1c42641b
necromaner <necromaner@foxmail.com>
2018-10-16 19:30:23 +0800

    10.16

---
+++ module/T5/learn.h
@@ -0,0 +1,23 @@
+//
+// Created by 温亚奇 on 2018/10/15.
+//
+
+#ifndef UNIT_TEST_LEARN_H
+#define UNIT_TEST_LEARN_H
+
+#include <iostream>
+using namespace std;
+
+void t1();
+int t1_1();
+
+
+
+
+
+
+
+
+
+
+#endif //UNIT_TEST_LEARN_H
--- module/T5/main.cpp
+++ module/T5/main.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
 #include <gtest/gtest.h>
 #include "Output.h"
-using namespace std;
-
+#include "learn.h"
+TEST(testCase, test0){
+    EXPECT_EQ(14, t1_1());
+}
 int main(int argc, char ** argv) {
-    int arr[] = {6, 7, 8, 9 ,10,11};
-    int* ptr = arr;
-//    *(++ptr) = 123;
-    int size=(int)sizeof(arr)/ (int)sizeof(arr[0]);
-    printf("%d---%d---%d\n",(int)sizeof(arr), (int)sizeof(arr[0]),size);
-    cout<<*ptr<<endl;
+    
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();  // 执行所有的 test case
 }
