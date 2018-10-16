7ec800c5509463fec9bdbd24b48ae3d2f42c9cec
necromaner <necromaner@foxmail.com>
2018-10-16 22:41:05 +0800

    10.16

--- module/T5/main.cpp
+++ module/T5/main.cpp
@@ -4,13 +4,27 @@
 TEST(testCase, test0){
     EXPECT_EQ(1, t1_1());
 }
+
+class A
+{
+public:
+    virtual void print(){cout<<"This is A"<<endl;}
+};
+class B : public A
+{
+public:
+    void print(){cout<<"This is B"<<endl;}
+};
 int main(int argc, char ** argv) {
     Circle circle(3);
     outPut(circle.Area());
     
-    
     Time time1(10,1);
     time1.show();
+    
+    
+    
+    
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();  // 执行所有的 test case
 }
